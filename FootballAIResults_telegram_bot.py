ae3d7ef928a61d2c29de3c26cee0b165ca10ee00
EnrMP23 <enrmp2002@gmail.com>
2024-10-04 14:32:08 -0700

    Update FootballAIResults_telegram_bot.py

--- FootballAIResults_telegram_bot.py
+++ FootballAIResults_telegram_bot.py
@@ -269,7 +269,7 @@ if __name__ == '__main__':
     application.add_handler(CommandHandler("predict", predict))
     application.run_webhook(
         listen="0.0.0.0",
-        port=5000,
+        port=8443,
         url_path="/webhook",
         webhook_url=WEBHOOK_URL
     )
