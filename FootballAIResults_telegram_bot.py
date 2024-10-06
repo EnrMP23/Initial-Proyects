3ecb7003d8b443eff3cb5ebd535e71708a95708a
EnrMP23 <enrmp2002@gmail.com>
2024-10-05 19:15:13 -0700

    Update FootballAIResults_telegram_bot.py

--- FootballAIResults_telegram_bot.py
+++ FootballAIResults_telegram_bot.py
@@ -253,8 +253,8 @@ async def predict(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
             # Mostrar estadísticas avanzadas
             advanced_stats_text = (
-                f"\nEstadísticas de lo que va de Temporada:\n"
-                f"{home_team_name} - Goles: {home_stats['goalsFor']}, Goles en Contra: {home_stats['goalsAgainst']}, Puntos: {home_stats['points']}"
+                f"Estadísticas de lo que va de Temporada:"
+                f"\n{home_team_name} - Goles: {home_stats['goalsFor']}, Goles en Contra: {home_stats['goalsAgainst']}, Puntos: {home_stats['points']}"
                 f"\n{away_team_name} - Goles: {away_stats['goalsFor']}, Goles en Contra: {away_stats['goalsAgainst']}, Puntos: {away_stats['points']}"
             )
             await update.message.reply_text(advanced_stats_text)
